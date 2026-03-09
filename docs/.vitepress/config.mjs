a3fdbc25382d8ee1892a867608f5b9ca20dbd70b
zixiangme <2742974891@qq.com>
2026-03-09 10:33:24 +0800

    Fix GitHub Pages base path

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vitepress'
 
 export default defineConfig({
+  base: '/mini-pi-soccer-docs/',
   title: 'Mini Pi+ Soccer',
   description: 'Mini Pi+ 足球应用开发指南',
   lang: 'zh-CN',
